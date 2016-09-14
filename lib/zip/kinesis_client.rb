85dc8b2435dbec19bb0e7e1caee3e8ed415f8dc1
Vivek A. Ganesan <onevivek@yahoo.com>
2016-09-14 10:22:22 +0000

    Updates

--- lib/zip/kinesis_client.rb
+++ lib/zip/kinesis_client.rb
@@ -3,30 +3,38 @@ require "aws-sdk"
 module Zip
   class KinesisClient
 
-     attr_reader :aws_region_name, :stream_name, :aws_key, :aws_secret, :client
+    attr_reader :stream_name
+    attr_reader :aws_region_name, :aws_key, :aws_secret, :client
 
-     def initialize(aws_region_name, stream_name, aws_key, aws_secret)
-       @stream_name = stream_name
-       @client = Aws::Kinesis::Client.new(region: aws_region_name, access_key_id: aws_key, secret_access_key: aws_secret)
-     end
+    def initialize(stream_name, aws_region=nil)
+      @stream_name = stream_name
+      raise "Invalid stream name '#{stream_name}' " unless stream_name =~ /^\S+$/
+      # AWS Region is the only required parameter
+      # Ideally, the key, and secret should be in credentials file
+      aws_region = ENV['AWS_REGION'] if aws_region.nil?
+      raise "Missing AWS region" unless aws_region
+      raise "Invalid AWS region '#{aws_region}'" unless aws_region =~ /^\S+$/
+      @client = Aws::Kinesis::Client.new({region: aws_region})
+    end
 
-     def publish(event_subject, event_timestamp, event)
-       formatted_event = validate_and_format(event)
-       @client.put_records({
-         stream_name: @stream_name,
-         records: [{
-           data: {event_subject: event_subject,
-                  event_timestamp: event_timestamp,
-                  event_metadata: formatted_event}.to_json, 
-           partition_key: "part_00"
-         }]
-       })
-     end
-   
-     def validate_and_format(event)
-       raise "Invalid format. Please refer documentation" if event.class != Hash
-       event.inject({}) {|hash, (k, v)|  hash[k.to_s.gsub(" ", "_")] = v; hash }
-     end
+    def validate_and_format(event)
+      raise "Invalid format. Please refer documentation" if event.class != Hash
+      event.inject({}) {|hash, (k, v)|  hash[k.to_s.gsub(" ", "_")] = v; hash }
+    end
 
+    def publish(event_subject, event_timestamp, event, partition_key = "part_00")
+      formatted_event = validate_and_format(event)
+      @client.put_records({
+        stream_name: @stream_name,
+        records: [{
+          data: {
+            event_subject: event_subject,
+            event_timestamp: event_timestamp,
+            event_metadata: formatted_event
+          }.to_json,
+          partition_key: partition_key
+        }]
+      })
+    end
   end
 end
--- test/zip_test.rb
+++ test/zip_test.rb
@@ -5,7 +5,35 @@ class ZipTest < Minitest::Test
     refute_nil ::Zip::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_publish_and_consume
+    stream_name = ENV['AWS_KINESIS_STREAM_NAME']
+    raise "Missing environment variable 'AWS_KINESIS_STREAM_NAME'" unless stream_name
+    aws_region_name = ENV['AWS_REGION_NAME']
+    raise "Missing environment variable 'AWS_REGION_NAME'" unless aws_region_name
+    kc = Zip::KinesisClient.new(stream_name, aws_region_name)
+    kc.publish("test_event_subject", Time.now, {"key" => "val"})
+    sleep 5
+    shard_index = 0
+    resp = kc.client.describe_stream({
+      stream_name: stream_name
+    })
+    sd = resp.stream_description
+    shards = sd.shards
+    shard = shards[shard_index]
+    shard_id = shard.shard_id
+    puts "shard_id = '#{shard_id}'"
+    resp = kc.client.get_shard_iterator({
+      stream_name: stream_name,
+      shard_id: shard_id,
+      shard_iterator_type: "LATEST"
+    })
+    shard_iterator = resp.shard_iterator
+    puts "shard_iterator = '#{shard_iterator}'"
+    resp = kc.client.get_records({
+      shard_iterator: shard_iterator,
+      limit: 1
+    })
+    puts resp.records.inspect
+    assert true
   end
 end
